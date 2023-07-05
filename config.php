a3fd9cac91ad2b1aeab0001e878ad239176c04ea
Hamdanafif <hamdanafiftsm@gmail.com>
2023-07-05 16:53:00 +0700

    first commit

---
+++ config.php
@@ -0,0 +1,36 @@
+<?php
+/*
+  | Source Code Aplikasi Penjualan Barang Kasir dengan PHP & MYSQL
+  | 
+  | @package   : pos-kasir-php
+  | @file	   : config.php ( untuk mengatur koneksi php ke database mysql )
+  | @author    : Hamdan1999 / Hamdan Afif
+  | 
+  | 
+  |
+  | 
+  | keterangan : untuk login aplikasi dengan username : admin dan password : 123
+  | 
+  | 
+  | 
+ */
+
+date_default_timezone_set("Asia/Jakarta");
+error_reporting(0);
+
+	// sesuaikan dengan server anda
+	$host 	= 'localhost'; // host server
+	$user 	= 'root';  // username server
+	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
+	$dbname = 'pos_kasir'; // nama database anda
+	
+	try{
+		$config = new PDO("mysql:host=$host;dbname=$dbname;", $user,$pass);
+		//echo 'sukses';
+	}catch(PDOException $e){
+		echo 'KONEKSI GAGAL' .$e -> getMessage();
+	}
+	
+	$view = 'fungsi/view/view.php'; // direktori fungsi select data
+?>
+
